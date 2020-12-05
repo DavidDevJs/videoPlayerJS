a2d43bf2325c1564d54e0dbfd96026f53dca32c2
David G <davidgonzalez0519@gmail.com>
2020-12-04 19:16:17 -0500

    first commit: design and elemnts finisheds, functionality missing in button pause

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('hola todo bien!')
+const video = document.querySelector('video')
+const button = document.querySelector('button')
+        
+function MediaPlayer(config) {
+    this.media = config.el;
+}
+
+MediaPlayer.prototype.play = function(){this.media.play()}
+
+
+
+const player = new MediaPlayer({el: video});
+button.onclick = () => player.play();
