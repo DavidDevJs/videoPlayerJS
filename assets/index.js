b2163486cfd737f6b5ce196a36b8286cf8a38ad3
David Gonzalez <davidgonzalez0519@gmail.com>
2021-01-19 16:11:52 -0500

    added Service Worker

--- assets/index.js
+++ assets/index.js
@@ -11,3 +11,9 @@ const buttonPlay = document.querySelector("#buttonPlay");
 const buttonMute = document.querySelector("#buttonMute");
 buttonPlay.onclick = () => player.togglePlay();
 buttonMute.onclick = () => player.toggleMute();
+
+if("serviceWorker" in navigator){
+   navigator.serviceWorker.register("../sw.js").catch(error => {
+     console.log(error.message);
+   });
+}
